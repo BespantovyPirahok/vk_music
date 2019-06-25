935a3d5cfa1ef2fcb8a5837f5ab604e5c1dcaa3f
violet1one <danya.violet@gmail.com>
2019-06-25 23:32:44 +0300

    Update and rename pikabu.py to vk_download_music.py

---
+++ vk_download_music.py
@@ -0,0 +1,72 @@
+#! /usr/bin/python3
+#  -*- coding: UTF-8 -*-
+
+# pip install request
+# pip install vk_api
+# pip install BeautifulSoup4
+
+import datetime
+import os
+import os.path
+
+import requests  
+import vk_api  
+from vk_api import audio
+
+vk_file = 'vk_config.v2.json'
+
+vk_login = ''  # Номер телефона указанный в настройках профиля ВК
+vk_password = ''  # Пароль ВК
+vk_id = ''  # id ВК
+path = r'C:\Users\%user%\Downloads\\' + 'music_vk'  # Путь где будет создана папка music_vk
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+
+# Если включена функция подтверждения входа
+def auth_handler():
+    code = input('Введите код подтверждения входа: ')
+    remember_device = False
+    return code, remember_device
+
+
+def main():
+    vk_session = vk_api.VkApi(login=vk_login, password=vk_password, auth_handler=auth_handler)
+    try:
+        vk_session.auth()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk_session.get_api()
+    vk_audio = audio.VkAudio(vk_session)
+    os.chdir(path)
+
+    i = vk_audio.get(owner_id=vk_id)[0]
+    r = requests.get(i['url'])
+    if r.status_code == 200:
+        try:
+            with open(i['artist'] + ' - ' + i['title'] + '.mp3', 'wb') as output_file:
+                output_file.write(r.content)
+        except OSError:
+            with open(i['artist'] + ' - ' + i['title'] + '.mp3', 'wb') as output_file:
+                output_file.write(r.content)
+    a = 0
+    time_start = datetime.datetime.now()
+    print('Начало загрузки', datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
+    for i in vk_audio.get(owner_id=vk_id):
+        try:
+            a += 1
+            r = requests.get(i['url'])
+            if r.status_code == 200:
+                with open(i['artist'] + ' - ' + i['title'] + '.mp3', 'wb') as output_file:
+                    output_file.write(r.content)
+        except OSError:
+            print('Ошибка загрузки: песня №', a, i['artist'] + ' - ' + i['title'])
+    time_end = datetime.datetime.now()
+    print('Загружено', len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))]),
+          'песен за', (time_end - time_start))
+
+
+if __name__ == '__main__':
+    main()
