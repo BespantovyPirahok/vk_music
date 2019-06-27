713834c214d9e503efd1cec7a74ed48d5dfe8955
violet1one <danya.violet@gmail.com>
2019-06-27 18:56:28 +0300

    Update vk__music.py

--- vk__music.py
+++ vk__music.py
@@ -15,20 +15,15 @@ from vk_api import audio
 
 from tqdm import tqdm
 
-vk_file = 'vk_config.v2.json'
-
-path = os.path.expanduser(r'~\Downloads') + r'\music_vk'
-print('Путь загрузки:', path)
-
-if not os.path.exists(path):
-    os.makedirs(path)
-
 
 # Авторизация
-def auth():
-    vk_login = input('Введите телефон или email: ')
-    vk_password = input('Введите пароль: ')
-    vk_id = input('Введите id: ')
+def auth(vk_login=None, vk_password=None, vk_id=None):
+    if not vk_login:
+        vk_login = input('Введите телефон или email: ')
+    if not vk_password:
+        vk_password = input('Введите пароль: ')
+    if not vk_id:
+        vk_id = input('Введите id: ')
     return vk_login, vk_password, vk_id
 
 
@@ -39,14 +34,23 @@ def two_step_auth():
     return code, remember_device
 
 
-def main():
-    vk_login, vk_password, vk_id = auth()
+def folder():
+    path = os.path.expanduser(r'~\Downloads') + r'\music_vk'
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+def main(vk_login=None, vk_password=None, vk_id=None):
+    vk_login, vk_password, vk_id = auth(vk_login=vk_login, vk_password=vk_password, vk_id=vk_id)
     vk_session = vk_api.VkApi(login=vk_login, password=vk_password, auth_handler=two_step_auth)
     try:
         vk_session.auth()
         print('Авторизация')
         vk_session.get_api()
         vk_audio = audio.VkAudio(vk_session)
+        path = folder()
         os.chdir(path)
 
         i = vk_audio.get(owner_id=vk_id)[0]
@@ -54,33 +58,37 @@ def main():
 
         if r.status_code == 200:
             print('Успех')
-            try:
-                song = 0
-                time_start = datetime.datetime.now()
-                print('Начало загрузки', datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
-                for i in vk_audio.get(owner_id=vk_id):
-                    try:
-                        song += 1
-                        r = requests.get(i['url'], stream=True)
-                        size = int(r.headers['content-length'])
-                        if r.status_code == 200:
-                            with open(str(song) + '_' + i['artist'] + ' - ' + i['title'] + '.mp3', 'wb') as output_file:
-                                print('Загрузка:', i['artist'] + ' - ' + i['title'])
-                                for data in tqdm(iterable=r.iter_content(chunk_size=1024), total=size / 1024,
-                                                 unit='KB'):
-                                    output_file.write(data)
-                    except OSError:
-                        print('Ошибка загрузки:', song, i['artist'] + ' - ' + i['title'])
-                time_end = datetime.datetime.now()
-                print('Загружено', len(next(os.walk(path))[2]), 'песен за', (time_end - time_start))
-                input('Нажмите ENTER для выхода')
-            except vk_api.AuthError as error_msg:
-                print(error_msg)
-                input('Нажмите ENTER для выхода')
-                return
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
+            song = 0
+            time_start = datetime.datetime.now()
+            print('Начало загрузки', datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
+            print('Путь загрузки:', path)
+            for i in vk_audio.get(owner_id=vk_id):
+                try:
+                    song += 1
+                    r = requests.get(i['url'], stream=True)
+                    size = int(r.headers['content-length'])
+                    if r.status_code == 200:
+                        with open(str(song) + '_' + i['artist'] + ' - ' + i['title'] + '.mp3', 'wb') as output_file:
+                            print('Загрузка:', i['artist'] + ' - ' + i['title'])
+                            for data in tqdm(iterable=r.iter_content(chunk_size=1024), total=size / 1024, unit='KB'):
+                                output_file.write(data)
+                except OSError:
+                    print('Ошибка загрузки:', song, i['artist'] + ' - ' + i['title'])
+            time_end = datetime.datetime.now()
+            print('Загружено', len(next(os.walk(path))[2]), 'песен за', (time_end - time_start))
+            input('Нажмите ENTER для выхода')
+        else:
+            print('Что-то пошло не так')
+            input('Нажмите ENTER для выхода')
+    except vk_api.AuthError:
+        print('Неверный пароль')
+        main(vk_login=vk_login, vk_id=vk_id)
+    except vk_api.exceptions.Captcha:
+        print('Неверный логин')
+        main(vk_password=vk_password, vk_id=vk_id)
+    except vk_api.exceptions.AccessDenied:
+        print('У Вас нет прав для просмотра аудио пользователя')
+        main(vk_login=vk_login, vk_password=vk_password)
 
 
 if __name__ == '__main__':
